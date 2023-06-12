c06a291242d40e19701d39ea2bef99a3bf1c4d10
sonyn2741 <86563327+sonyn2741@users.noreply.github.com>
2023-06-12 09:49:15 +0700

    Update setup.py

--- PythonAPI/setup.py
+++ PythonAPI/setup.py
@@ -20,7 +20,7 @@ ext_modules = [
 setup(name='pycocotools',
       packages=['pycocotools'],
       package_dir = {'pycocotools': 'pycocotools'},
-      version='2.0.4',
+      version='2.0.6',
       ext_modules=
           cythonize(ext_modules)
       )
